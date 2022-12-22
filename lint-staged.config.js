a66c5c379c3f75451cde8b791405d20686029729
Dafnik <git@dafnik.me>
2022-12-22 16:40:19 +0100

    ci: fix lint staged typo

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   '{apps,libs,tools}/**/*.{ts,js,json,md,html,css,scss}': [
-    'nx affected --target lint --fix true --uncommitted',
+    'nx affected --target lint --uncommitted --fix true',
     //'nx affected --target test --uncommited',
     'nx format:write --uncommitted',
   ],

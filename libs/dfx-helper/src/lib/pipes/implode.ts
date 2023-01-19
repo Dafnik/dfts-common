024c5b067ec4d276575d2b20cbc18bb67b441a45
Dafnik <git@dafnik.me>
2023-01-19 13:42:14 +0100

    feat(dfx-helper): add pluckedSource to imploder

--- libs/dfx-helper/src/lib/pipes/implode.ts
+++ libs/dfx-helper/src/lib/pipes/implode.ts
@@ -20,6 +20,7 @@ export class DfxImplode implements PipeTransform {
 @Pipe({
   name: 'implodeMapped',
   standalone: true,
+  pure: true,
 })
 export class DfxImplodeMapped implements PipeTransform {
   transform<T>(
@@ -33,7 +34,24 @@ export class DfxImplodeMapped implements PipeTransform {
   }
 }
 
-const PIPES = [DfxImplode, DfxImplodeMapped];
+@Pipe({
+  name: 'implodePluck',
+  standalone: true,
+  pure: true,
+})
+export class DfxImplodePlucked implements PipeTransform {
+  transform<T>(
+    strings: UndefinedOrNullOr<T[]>,
+    key: UndefinedOrNullOr<keyof T>,
+    separator?: UndefinedOrNullOr<string>,
+    maxLength?: UndefinedOrNullOr<number>,
+    suffix?: UndefinedOrNullOr<string>
+  ): string {
+    return s_imploder().pluckedSource(strings, key).maxLength(maxLength).separator(separator).suffix(suffix).build();
+  }
+}
+
+const PIPES = [DfxImplode, DfxImplodeMapped, DfxImplodePlucked];
 
 @NgModule({
   imports: PIPES,

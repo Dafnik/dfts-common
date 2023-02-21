1a780c9c2dd98dbe15885a8618822c8877de3980
Dafnik <git@dafnik.me>
2023-02-21 15:05:22 +0100

    feat(dfx-helper): add new modular service interfaces

---
+++ libs/dfx-helper/src/lib/entities/entities.ts
@@ -0,0 +1,28 @@
+import {Observable} from 'rxjs';
+import {IHasID} from 'dfts-helper';
+
+export interface HasGetAll<T> {
+  getAll$(): Observable<T[]>;
+}
+
+export interface HasGetByParent<EntityType, ParentType extends IHasID<ParentType['id']>> {
+  getByParent$(id: ParentType['id']): Observable<EntityType[]>;
+}
+
+export interface HasGetSingle<T extends IHasID<T['id']>> {
+  getSingle$(id: T['id']): Observable<T>;
+}
+
+export interface HasDelete<T extends IHasID<T['id']>> {
+  delete$(id: T['id']): Observable<unknown>;
+}
+
+export type HasCreate<CreateDTOType, ResponseType extends IHasID<ResponseType['id']>> = {
+  create$(dto: CreateDTOType): Observable<ResponseType>;
+};
+
+export interface HasUpdate<UpdateDTOType extends IHasID<UpdateDTOType['id']>, ResponseType extends IHasID<ResponseType['id']>> {
+  update$(dto: UpdateDTOType): Observable<ResponseType>;
+}
+
+export type HasIdAndNumber<ID> = IHasID<ID> & {number: number};

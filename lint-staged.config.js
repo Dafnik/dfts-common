38f1653fd28108268be3ede9026938a305a86d0c
Dafnik <git@dafnik.me>
2023-05-29 15:12:27 +0200

    ci: add dfts-qrcode and dfx-qrcode release workflows

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '{apps,libs,tools}/**/*.{ts,js,json,md,html,css,scss}': [
+  '*.{ts,js,json,md,html,css,scss}': [
     'nx affected --target lint --uncommitted --fix true',
     //'nx affected --target test --uncommited',
     'nx format:write --uncommitted',

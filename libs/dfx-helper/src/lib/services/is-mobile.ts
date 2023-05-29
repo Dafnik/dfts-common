29695a7b4e0052d5edc0a4fcd02bfb262b67c4fc
Dafnik <git@dafnik.me>
2023-05-29 14:40:18 +0200

    fix(dfx-helper): is mobile service not firing at start

--- libs/dfx-helper/src/lib/services/is-mobile.ts
+++ libs/dfx-helper/src/lib/services/is-mobile.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable, NgZone, Provider} from '@angular/core';
 import {ViewportRuler} from '@angular/cdk/overlay';
-import {distinctUntilChanged, map, shareReplay} from 'rxjs';
+import {distinctUntilChanged, map, shareReplay, startWith} from 'rxjs';
 import {HELPER_MOBILE_BREAKPOINT} from '../config';
 
 @Injectable()
@@ -9,11 +9,11 @@ export class IsMobileService {
   private readonly ngZone = inject(NgZone);
   private readonly viewportRuler = inject(ViewportRuler);
 
-  public isMobile$ = this.viewportRuler.change(200).pipe(
-    map(() => this.viewportRuler.getViewportSize().width <= this.isMobileBreakpoint),
-    distinctUntilChanged(),
-    shareReplay(1)
-  );
+  private isMobile = () => this.viewportRuler.getViewportSize().width <= this.isMobileBreakpoint;
+
+  public isMobile$ = this.viewportRuler
+    .change(200)
+    .pipe(map(this.isMobile), startWith(this.isMobile()), distinctUntilChanged(), shareReplay(1));
 }
 
 export function injectIsMobile$() {

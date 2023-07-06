25d9ae82246e2cf348468104397dfd012eaa9414
Dafnik <git@dafnik.me>
2023-07-06 14:49:37 +0200

    feat(dfx-helper): add back directive

---
+++ libs/dfx-helper/src/lib/directives/back.ts
@@ -0,0 +1,29 @@
+import {Location} from '@angular/common';
+import {Directive, HostListener, inject, Input} from '@angular/core';
+import {Router} from '@angular/router';
+import {injectWindow} from '../windows-provider';
+
+@Directive({
+  selector: '[back]',
+  standalone: true,
+})
+export class DfxBackDirective {
+  location = inject(Location);
+  router = inject(Router);
+  window = injectWindow();
+
+  @Input() fallbackUrl = '/';
+
+  @HostListener('click')
+  goBack(): void {
+    if (this.window) {
+      if (this.window.history.length > 1) {
+        this.location.back();
+      } else {
+        this.window?.close();
+      }
+    } else {
+      void this.router.navigateByUrl(this.fallbackUrl);
+    }
+  }
+}

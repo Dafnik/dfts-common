bebbb7a0ab1271787a2685bf212b09353eb7299f
Dafnik <git@dafnik.me>
2023-07-11 09:57:24 +0200

    refactor(dfx-helper): remove unused variable

--- libs/dfx-helper/src/lib/services/is-mobile.ts
+++ libs/dfx-helper/src/lib/services/is-mobile.ts
@@ -1,4 +1,4 @@
-import {inject, Injectable, NgZone, Provider} from '@angular/core';
+import {inject, Injectable, Provider} from '@angular/core';
 import {ViewportRuler} from '@angular/cdk/overlay';
 import {distinctUntilChanged, map, shareReplay, startWith} from 'rxjs';
 import {HELPER_MOBILE_BREAKPOINT} from '../config';
@@ -6,13 +6,12 @@ import {HELPER_MOBILE_BREAKPOINT} from '../config';
 @Injectable()
 export class IsMobileService {
   private readonly isMobileBreakpoint = inject(HELPER_MOBILE_BREAKPOINT);
-  private readonly ngZone = inject(NgZone);
   private readonly viewportRuler = inject(ViewportRuler);
 
   private isMobile = () => this.viewportRuler.getViewportSize().width <= this.isMobileBreakpoint;
 
   public isMobile$ = this.viewportRuler
-    .change(200)
+    .change(50)
     .pipe(map(this.isMobile), startWith(this.isMobile()), distinctUntilChanged(), shareReplay(1));
 }
 

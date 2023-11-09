6c435a23d19a7d4d469df903967b04c940059d24
Dafnik <git@dafnik.me>
2023-11-09 19:37:10 +0100

    fix: demo routing not working

--- apps/dfx-bootstrap-table-demo/src/app/app.component.ts
+++ apps/dfx-bootstrap-table-demo/src/app/app.component.ts
@@ -6,11 +6,11 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
   template: `
     <div class="p-5">
       <div class="d-flex gap-3 align-items-center mb-4">
-        <a class="btn btn-primary" href="all">Everything (filtering, ordering and pagination</a>
-        <a class="btn btn-primary" href="simple">Simple</a>
-        <a class="btn btn-primary" href="ordering">Ordering</a>
-        <a class="btn btn-primary" href="pagination">Pagination</a>
-        <a class="btn btn-primary" href="filtering">Filtering</a>
+        <a class="btn btn-primary" routerLink="all">Everything (filtering, ordering and pagination</a>
+        <a class="btn btn-primary" routerLink="simple">Simple</a>
+        <a class="btn btn-primary" routerLink="ordering">Ordering</a>
+        <a class="btn btn-primary" routerLink="pagination">Pagination</a>
+        <a class="btn btn-primary" routerLink="filtering">Filtering</a>
 
         <theme-picker />
       </div>

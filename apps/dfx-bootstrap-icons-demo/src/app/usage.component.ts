1c36111a3b447deed9ab89285ad7c26992f8e27f
Dafnik <git@dafnik.me>
2023-11-09 09:51:06 +0100

    feat: make usage better
    fix: rollback to webpack builder

--- apps/dfx-bootstrap-icons-demo/src/app/usage.component.ts
+++ apps/dfx-bootstrap-icons-demo/src/app/usage.component.ts
@@ -8,48 +8,132 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-usage',
   template: `
-      <div class="d-flex flex-column gap-2">
-          <h2>Usage</h2>
-          <span>The component renders an <code>svg</code>.</span>
-
-          <h3 class="mt-4">Configuration</h3>
-          <div class="d-flex flex-column gap-5">
-              <div class="d-flex">
-                  <div class="col-12 col-lg-4">
-                      <h4>Standalone (recommended)</h4>
-                  </div>
-                  <div class="col-12 col-lg-8">
-                      <div class="d-flex w-100 rounded px-4 pt-3 font-monospace" style="background-color:#343A40 ">
-                          <pre><code [innerHTML]="standaloneConfigCode"></code></pre>
-                      </div>
-                  </div>
+    <div class="d-flex flex-column gap-5">
+      <div>
+        <h3>Usage</h3>
+        <p>You can use dfx-boostrap-icons in two ways.</p>
+        <div class="d-flex flex-column gap-5 mt-4">
+          <div class="d-flex">
+            <div class="col-12 col-lg-4">
+              <h4>Standalone</h4>
+              <p>Directly provide icons on configuration.</p>
+            </div>
+            <div class="col-12 col-lg-8">
+              <div class="d-flex w-100 rounded px-4 pt-3 font-monospace" style="background-color:#343A40 ">
+                <pre><code [innerText]="importStandaloneCode"></code></pre>
               </div>
-              <div class="d-flex">
-                  <div class="col-12 col-lg-4">
-                      <h4>Module</h4>
-                  </div>
-                  <div class="col-12 col-lg-8">
-                      <div class="d-flex w-100 rounded px-4 pt-3 font-monospace" style="background-color:#343A40 ">
-                          <pre><code [innerHTML]="moduleConfigCode"></code></pre>
-                      </div>
-                  </div>
-              </div>
-              <div class="d-flex">
-                  <div class="col-12 col-lg-4">
-                      <h4>Provide Icons in a standalone component</h4>
-                  </div>
-                  <div class="col-12 col-lg-8">
-                      <div class="d-flex w-100 rounded px-4 pt-3 font-monospace" style="background-color:#343A40 ">
-                          <pre><code [innerHTML]="standaloneComponentConfigCode"></code></pre>
-                      </div>
-                  </div>
-              </div>
+            </div>
           </div>
+          <div class="d-flex">
+            <div class="col-12 col-lg-4">
+              <h4>Module</h4>
+              <p>Directly provide icons on configuration.</p>
+            </div>
+            <div class="col-12 col-lg-8">
+              <div class="d-flex w-100 rounded px-4 pt-3 font-monospace" style="background-color:#343A40 ">
+                <pre><code [innerText]="importModuleCode"></code></pre>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <div>
+        <h3>Importing icons</h3>
+        <p>You can import icons in a few ways.</p>
+        <div class="d-flex flex-column gap-5 mt-4">
+          <div class="d-flex">
+            <div class="col-12 col-lg-4">
+              <h4>Standalone with configuration</h4>
+              <p>Directly provide icons on configuration.</p>
+            </div>
+            <div class="col-12 col-lg-8">
+              <div class="d-flex w-100 rounded px-4 pt-3 font-monospace" style="background-color:#343A40 ">
+                <pre><code [innerText]="importStandaloneCode"></code></pre>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex">
+            <div class="col-12 col-lg-4">
+              <h4>Module with configuration</h4>
+              <p>Directly provide icons on configuration.</p>
+            </div>
+            <div class="col-12 col-lg-8">
+              <div class="d-flex w-100 rounded px-4 pt-3 font-monospace" style="background-color:#343A40 ">
+                <pre><code [innerText]="importModuleCode"></code></pre>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex">
+            <div class="col-12 col-lg-4">
+              <h4>Standalone</h4>
+              <p>Just provide the items you need per component.</p>
+            </div>
+            <div class="col-12 col-lg-8">
+              <div class="d-flex w-100 rounded px-4 pt-3 font-monospace" style="background-color:#343A40 ">
+                <pre><code [innerText]="importStandaloneComponentCode"></code></pre>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3 class="mt-5">Configuration</h3>
+        <p>You can configure <code>dfx-bootstrap-icons</code> in two ways. Note that you DO NOT have to configure the library.</p>
+        <div class="d-flex flex-column gap-5 mt-4">
+          <div class="d-flex">
+            <div class="col-12 col-lg-4">
+              <h4>Standalone (recommended)</h4>
+              <p>Configure the library wherever you have a providers array.</p>
+            </div>
+            <div class="col-12 col-lg-8">
+              <div class="d-flex w-100 rounded px-4 pt-3 font-monospace" style="background-color:#343A40 ">
+                <pre><code [innerText]="standaloneConfigCode"></code></pre>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex">
+            <div class="col-12 col-lg-4">
+              <h4>Module</h4>
+              <p>Configure the library in any module you like.</p>
+            </div>
+            <div class="col-12 col-lg-8">
+              <div class="d-flex w-100 rounded px-4 pt-3 font-monospace" style="background-color:#343A40 ">
+                <pre><code [innerText]="moduleConfigCode"></code></pre>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   `
 })
 export class UsageComponent {
-  standaloneConfigCode = `import { allIcons, provideIcons, withHeight, withWidth } from "dfx-bootstrap-icons";
+  standaloneConfigCode = `import { provideDfxBootstrapIcons, withHeight, withWidth } from "dfx-bootstrap-icons";
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideDfxBootstrapIcons(
+      withWidth('32'),
+      withHeight('48')
+    )
+  ]
+})`;
+
+  moduleConfigCode = `import { DfxIconsModule, withHeight, withWidth } from "dfx-bootstrap-icons";
+
+@NgModule({
+  imports: [
+    DfxIconsModule.setup(
+      withWidth('32'),
+      withHeight('48')
+    )
+  ],
+  ...
+})
+export class AppModule { }`;
+
+  importStandaloneCode = `import { allIcons, provideDfxBootstrapIcons, withIcons, withWidth, withHeight } from "dfx-bootstrap-icons";
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -61,7 +145,8 @@ bootstrapApplication(AppComponent, {
   ]
 })`;
 
-  moduleConfigCode = `import { allIcons, DfxIconsModule, withHeight, withWidth } from "dfx-bootstrap-icons";
+
+  importModuleCode = `import { DfxIconsModule, withHeight, withWidth, withIcons, allIcons } from "dfx-bootstrap-icons";
 
 @NgModule({
   imports: [
@@ -75,7 +160,7 @@ bootstrapApplication(AppComponent, {
 })
 export class AppModule { }`;
 
-  standaloneComponentConfigCode = `import { allIcons, IconComponent, provideIcons } from "dfx-bootstrap-icons";
+  importStandaloneComponentCode = `import { allIcons, IconComponent, provideIcons } from "dfx-bootstrap-icons";
 
 @Component({
   imports: [ IconComponent ],
@@ -83,7 +168,7 @@ export class AppModule { }`;
     provideIcons(allIcons), // provideIcons({ xCircleFill, zeroCircle })
   ],
   name: 'app-test-component',
-  ...
+  template: '<bi name="x-circle-fill" /> '
 })
 export class AppComponent { }
   `;

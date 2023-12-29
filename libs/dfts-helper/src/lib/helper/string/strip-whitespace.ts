3c944fa2dd40f544568e6709bf0a8e13bda7e783
Dafnik <git@dafnik.me>
2023-12-29 17:18:24 +0100

    fix(dfts-helper): strip whitespace

--- libs/dfts-helper/src/lib/helper/string/strip-whitespace.ts
+++ libs/dfts-helper/src/lib/helper/string/strip-whitespace.ts
@@ -1,3 +1,3 @@
 import { UndefinedOrNullOr } from '../../types';
 
-export const s_stripWhitespace = (text: UndefinedOrNullOr<string>): string => text?.trim().replace(/^\s+/, '').replace(/\s+$/, '') ?? '';
+export const s_stripWhitespace = (text: UndefinedOrNullOr<string>): string => text?.trim().replace(/\s/g, '') ?? '';

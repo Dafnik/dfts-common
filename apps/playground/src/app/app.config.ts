a454c7634c59659602223fde2a3c35220df2b233
Dafnik <git@dafnik.me>
2024-01-18 13:02:13 +0100

    feat: add base64 pdf converter

--- apps/playground/src/app/app.config.ts
+++ apps/playground/src/app/app.config.ts
@@ -1,9 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withEnabledBlockingInitialNavigation } from "@angular/router";
-import { appRoutes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(appRoutes, withEnabledBlockingInitialNavigation())
-  ],
+  providers: [],
 };
--- apps/playground/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Route } from '@angular/router';
-
-export const appRoutes: Route[] = [
-  { path: '', loadComponent: () => import('./home.component').then((c) => c.HomeComponent) },
-  { path: 'base64-pdf-converter', loadComponent: () => import('./base64-to-pdf-converter.component').then((c) => c.Base64ToPdfConverterComponent) },
-  { path: '**', redirectTo: '' },
-];

3b42af01d6df89506de05c5859a00822a42c8aec
Dafnik <git@dafnik.me>
2024-03-07 17:24:15 +0100

    revert: some changes from translate store

--- libs/dfx-translate/src/lib/service/set-language.ts
+++ libs/dfx-translate/src/lib/service/set-language.ts
@@ -2,7 +2,7 @@ import { isPlatformBrowser } from '@angular/common';
 import { inject, PLATFORM_ID } from '@angular/core';
 import { TranslateStore } from './translate.store';
 import { TRANSLATE_LOCALSTORAGE_KEY } from '../types';
-import { TRANSLATE_REMEMBER_LANGUAGE } from '../features';
+import { TRANSLATE_REMEMBER_LANGUAGE } from '../features/remember-language/remember-language';
 
 export function dfxTranslateSetLanguage(): (language: string) => void {
   const isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
@@ -10,7 +10,7 @@ export function dfxTranslateSetLanguage(): (language: string) => void {
   const store = inject(TranslateStore);
 
   return (language: string): void => {
-    store.selectedLanguage.set(language);
+    store.selectedLanguage.next(language);
     if (isBrowser && rememberLanguage) {
       localStorage.setItem(TRANSLATE_LOCALSTORAGE_KEY, language);
     }

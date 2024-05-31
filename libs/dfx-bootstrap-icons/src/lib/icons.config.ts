19423d5bf9ea43f2c174c567034a5f6aa4b3f8c6
Dafnik <git@dafnik.me>
2024-05-31 15:52:40 +0200

    fix(dfx-bootstrap-icons): use default size for injection

--- libs/dfx-bootstrap-icons/src/lib/icons.config.ts
+++ libs/dfx-bootstrap-icons/src/lib/icons.config.ts
@@ -13,11 +13,11 @@ export const DEFAULT_ICON_SIZE = '16';
 export const DEFAULT_COLOR = 'currentColor';
 
 export const ICON_WIDTH = new InjectionToken<string>('DFX_ICONS_WIDTH', {
-  factory: () => '16',
+  factory: () => DEFAULT_ICON_SIZE,
 });
 
 export const ICON_HEIGHT = new InjectionToken<string>('DFX_ICONS_HEIGHT', {
-  factory: () => '16',
+  factory: () => DEFAULT_ICON_SIZE,
 });
 
 export const ICON_COLOR = new InjectionToken<ColorValueHex | undefined>('DFX_ICONS_COLOR', {

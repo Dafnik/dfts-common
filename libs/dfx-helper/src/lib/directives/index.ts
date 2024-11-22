ad4df99b22343a6eaa7398df06c92e0f309f38ce
Dafnik <git@dafnik.me>
2024-11-23 00:29:15 +0100

    chore(dfx-helper): remove multiple things
    directives: count-up, ng-sub, ping
    
    entities-service: HasGetAll, HastGetParent, HasGetSingle, HasGetSelected, HasDelete
    
    pipes: StringPipesModule
    
    styles: dfx-helper.scss, a styling, btnSpinner

--- libs/dfx-helper/src/lib/directives/index.ts
+++ libs/dfx-helper/src/lib/directives/index.ts
@@ -1,7 +1,4 @@
 export * from './autofocus';
 export * from './back';
-export * from './count-up';
-export * from './ng-sub';
 export * from './online-offline';
-export * from './ping';
 export * from './stop-propagation';
--- libs/dfx-helper/src/lib/entities/entities-service.interface.ts
+++ libs/dfx-helper/src/lib/entities/entities-service.interface.ts
@@ -1,28 +1,6 @@
 import { Observable } from 'rxjs';
 import { IHasID } from 'dfts-helper';
 
-export interface HasGetAll<T> {
-  getAll$(): Observable<T[]>;
-}
-
-export interface HasGetByParent<EntityType, ParentType extends IHasID<ParentType['id']>> {
-  getByParent$(id: ParentType['id']): Observable<EntityType[]>;
-}
-
-export interface HasGetSingle<T extends IHasID<T['id']>> {
-  getSingle$(id: T['id']): Observable<T>;
-}
-
-export interface HasGetSelected<SelectedDTOType extends IHasID<SelectedDTOType['id']>> {
-  setSelected(it: SelectedDTOType | undefined): void;
-
-  getSelected$: Observable<SelectedDTOType | undefined>;
-}
-
-export interface HasDelete<T extends IHasID<T['id']>> {
-  delete$(id: T['id']): Observable<unknown>;
-}
-
 export type HasCreate<CreateDTOType, ResponseType extends IHasID<ResponseType['id']>> = {
   create$(dto: CreateDTOType): Observable<ResponseType>;
 };

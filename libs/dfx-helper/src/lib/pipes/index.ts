2708dc13d2229b8df255b1da5a8c51f9e3f6137b
Dafnik <git@dafnik.me>
2025-02-04 20:36:32 +0100

    feat(dfx-helper): add sanitize-html pipe

--- libs/dfx-helper/src/lib/pipes/index.ts
+++ libs/dfx-helper/src/lib/pipes/index.ts
@@ -3,5 +3,6 @@ export * from './currency.pipe';
 export * from './implode';
 export * from './name-map';
 export * from './pluck';
+export * from './sanitize-html.pipe';
 export * from './time-left';
 export * from './time-span';
---
+++ libs/dfx-helper/src/lib/pipes/sanitize-html.pipe.ts
@@ -0,0 +1,11 @@
+import { inject, Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+@Pipe({ name: 'sanitizeHtml', pure: true })
+export class SanitizeHtmlPipe implements PipeTransform {
+  private readonly _sanitizer = inject(DomSanitizer);
+
+  transform(value: string): SafeHtml {
+    return this._sanitizer.bypassSecurityTrustHtml(value);
+  }
+}

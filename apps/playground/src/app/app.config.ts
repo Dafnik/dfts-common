5ab48b2a69fd1dfebed338f89188724bd03eed0f
Dafnik <git@dafnik.me>
2025-06-06 15:36:49 +0200

    feat: migrate playground to astro page

--- apps/playground/src/app/app.config.ts
+++
@@ -1,6 +0,0 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideClientHydration, withEventReplay, withIncrementalHydration } from '@angular/platform-browser';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideClientHydration(withEventReplay(), withIncrementalHydration())],
-};

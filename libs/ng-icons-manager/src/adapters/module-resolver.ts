7a4b965fb23143d06949db6dea774e0e8fb13633
Dafnik <git@dafnik.me>
2025-12-03 16:02:18 +0100

    feat: change ng-icons-manager to NodeNext

--- libs/ng-icons-manager/src/adapters/module-resolver.ts
+++ libs/ng-icons-manager/src/adapters/module-resolver.ts
@@ -1,12 +1,8 @@
-import { createRequire } from 'module';
-
 import type { ModuleResolver } from '../types';
 
 export class RealModuleResolver implements ModuleResolver {
-  private cjsRequire = createRequire(import.meta.url);
-
   async resolve(packageName: string): Promise<Record<string, string>> {
-    const resolved = this.cjsRequire.resolve(packageName, {
+    const resolved = require.resolve(packageName, {
       paths: [process.cwd()],
     });
     return await import(resolved);
